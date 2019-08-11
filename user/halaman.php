60802f341195f7816a261c82689fc523beb4dfd8
r1eki <muhammadrakayusrifan@gmail.com>
2019-08-11 10:28:40 +0700

    update halaman

--- user/halaman.php
+++ user/halaman.php
@@ -88,7 +88,7 @@
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Fitur Counter</h6>
-            <a class="collapse-item" href="utilities-color.html">Counter</a>
+            <a class="collapse-item" href="?page=counter">Counter</a>
            </div>
         </div>
       </li>
